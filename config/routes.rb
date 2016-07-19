a63dcd526541cb24bd5d4129a99eb3a07b232b33
Christopher Spring <chris.spring@gmail.com>
2016-07-19 12:16:46 -0700

    controller code with some basic spec setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :letter_distributions, only: %i[new create]
+  resources :letter_distributions, only: %i[new create show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'letter_distributions#new'
